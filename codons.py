ae9fc17c5a9fa13c2492e6917c2f3790ded22fbe
Joud Rashed <joudrashed2808@gmail.com>
2025-12-06 18:43:23 +0200

    Implement create_codon_dict function
    Implemented create_codon_dict to read a file and create a codon dictionary.

--- codons.py
+++ codons.py
@@ -1,4 +1,11 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
-
+    codon_table = []
+    with open(file_path, "r") as fobj:
+        all_rows = fobj.readlines()
+    for entry in all_rows:
+        fields = entry.strip().split('\t')
+        if len(fields) >= 3:
+            key = fields[0]
+            value = fields[2]
+            codon_table[key] = value
+    return codon_table
